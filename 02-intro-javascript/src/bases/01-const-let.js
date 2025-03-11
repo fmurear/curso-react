fe59d8fbea650ccb47a2a9eccbd46985cfcf73d7
fmurear <pakito_699@hotmail.com>
2025-03-11 14:18:28 +0000

    Punto 1

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+const nombre = 'Francisco';
+const appellido = 'Muñoz-Reja Arias';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, appellido, valorDado);
+
+if(true){
+    const nombre = 'Peter'
+    console.log(nombre);
+}
+
+console.log( valorDado )
---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,8 @@
+
+const nombre =  'Francisco'
+const apellido = 'Muñoz-Reja'
+
+const nombreApellido = `Hola mundo 
+${nombre} 
+${apellido} 
+${ 1 + 1}`
