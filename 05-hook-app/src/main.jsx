5c6f0f7fa627776800d0f2c36bf0f060afb84f71
fmurear <pakito_699@hotmail.com>
2025-04-08 12:34:40 +0000

    Fin sección 10 useReducer

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import { Padre } from './07-tarea-memo/Padre'
+import { TodoApp } from './08-useReducer/TodoApp'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Padre />
-  </StrictMode>,
+  // <StrictMode>
+    <TodoApp />
+  // </StrictMode>
 )
