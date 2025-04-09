786a64d47ace44c87b4536c2dc4d4bfca5c7c215
fmurear <pakito_699@hotmail.com>
2025-04-09 10:56:44 +0000

    Fin sección 11 useContext

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
+import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <TodoApp />
-  // </StrictMode>
+  <BrowserRouter>
+  {/* <StrictMode> */}
+    <MainApp />
+  {/* </StrictMode> */}
+  </BrowserRouter>
 )
